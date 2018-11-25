8eb743eabf8f36998c69e0830e07cda8a962fbd8
Efim Poberezkin <efimpoberezkin@gmail.com>
2018-11-25 23:26:05 +0400

    Attempt to fail sonar

---
+++ src/main/java/org/springframework/samples/petclinic/CustomAppToBreakSonar.java
@@ -0,0 +1,9 @@
+package org.springframework.samples.petclinic;
+
+public class CustomAppToBreakSonar {
+
+    public static void main(String[] args) {
+        int i = 0;
+        i = i++;
+    }
+}
