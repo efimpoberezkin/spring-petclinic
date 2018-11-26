51aa256b6fc6e74382c68c8b60927644cba7d86f
Efim Poberezkin <efim@poberezkin.ru>
2018-11-26 13:57:54 +0400

    Fix sonar, fail tests

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -40,8 +40,8 @@ public class ValidatorTests {
         assertThat(constraintViolations.size()).isEqualTo(1);
         ConstraintViolation<Person> violation = constraintViolations.iterator().next();
         assertThat(violation.getPropertyPath().toString()).isEqualTo("firstName");
-//        assertThat(violation.getMessage()).isEqualTo("mustn't be empty");
-        assertThat(violation.getMessage()).isEqualTo("must not be empty");
+        assertThat(violation.getMessage()).isEqualTo("mustn't be empty");
+//        assertThat(violation.getMessage()).isEqualTo("must not be empty");
     }
 
 }
