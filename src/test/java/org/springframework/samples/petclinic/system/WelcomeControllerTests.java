7246a0e2ce4d4ba02f5cb2ad06826b96d9791e5e
Efim Poberezkin <efim@poberezkin.ru>
2018-12-07 21:05:53 +0400

    Increase test coverage

---
+++ src/test/java/org/springframework/samples/petclinic/system/WelcomeControllerTests.java
@@ -0,0 +1,30 @@
+package org.springframework.samples.petclinic.system;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Test class for {@link WelcomeController}
+ *
+ * @author Efim Poberezkin
+ */
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = WelcomeController.class)
+public class WelcomeControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testWelcome() throws Exception {
+        mockMvc.perform(get("/"))
+            .andExpect(status().isOk());
+    }
+}
